718dc8ab149c8c8755df324d45b07bf6dd728b38
nickrotondo <nickrotondo@gmail.com>
2020-11-04 18:21:49 -0800

    Add hopmepage and household connect screens

---
+++ screens/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Homepage() {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
