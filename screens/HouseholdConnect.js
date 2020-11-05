bcfa67e50d8240be836c62e366a44fb9ac7c5c53
nickrotondo <nickrotondo@gmail.com>
2020-11-04 18:23:11 -0800

    Add text

--- screens/HouseholdConnect.js
+++ screens/HouseholdConnect.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native'
 export default function HouseholdConnect() {
   return (
     <View>
-      <Text></Text>
+      <Text>This is the household chooser</Text>
     </View>
   )
 }
