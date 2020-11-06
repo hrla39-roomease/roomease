cd462abc3153e25fc30affb383638ccbe07e5631
alphinaa <alphina.long@gmail.com>
2020-11-05 19:16:57 -0800

    Added lists

--- screens/ExpenseNavigator.js
+++ screens/ExpenseNavigator.js
@@ -1,11 +1,46 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, Button, SafeAreaView, Alert, TextInput} from 'react-native';
+import { StyleSheet, Text, View, Image, Button, SafeAreaView, Alert, TextInput, TouchableOpacity, StatusBar, FlatList} from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import AddExpenseScreen  from './AddExpenseScreen.js';
+import colors from '../assets/colors.js';
 
 const Stack = createStackNavigator();
 
+const DATA = [
+  {
+    id: "1",
+    name: "Nick",
+  },
+  {
+    id: "2",
+    name: "Alvin",
+  },
+  {
+    id: "3",
+    name: "Alphina",
+  },
+];
+
+const Item = ({ item, onPress, style }) => (
+  <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
+    <Text style={styles.name}>{item.name}</Text>
+  </TouchableOpacity>
+);
+
 function HomeExpenseScreen({navigation}) {
+  const [selectedId, setSelectedId] = useState(null);
+  const renderItem = ({ item }) => {
+    const backgroundColor = item.id === selectedId ? colors.primaryBlue : colors.mediumGrey;
+
+    return (
+      <Item
+        item={item}
+        onPress={() => setSelectedId(item.id)}
+        style={{ backgroundColor }}
+      />
+    );
+  };
+
   return (
     <View style={styles.container}>
     <Text>Hello Expense Screen</Text>
@@ -15,10 +50,13 @@ function HomeExpenseScreen({navigation}) {
     title="Add an expense"
     onPress={() => navigation.navigate('Add Expense Screen')}
   />
+  <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        extraData={selectedId}
+      />
 
-  <Text>Nick, Water Bill, $50</Text>
-  <Text>Alvin, Electricity Bill, $109</Text>
-  <Text>Alphina, Bread, $4</Text>
   </View>
   )
 }
@@ -38,7 +76,14 @@ export default function ExpenseNavigator () {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  name: {
+    fontSize: 32,
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
 });
