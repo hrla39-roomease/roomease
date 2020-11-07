dfc8a4080071de32876e6ce1a79037d99c642edb
nickrotondo <nickrotondo@gmail.com>
2020-11-06 18:21:06 -0800

    Darken neutralDark

--- assets/colors.js
+++ assets/colors.js
@@ -1,10 +1,10 @@
 export default {
   primary: '#3A80C9', // Primary Blue
-  primaryLight: '#5C98D7', // Primary Light Blue
+  primaryLight: '#94B8DB', // Primary Light Blue
   primaryLighter: '#A8C3E9', // Even Lighter Blue
   primaryDark: '#3C556B', // Dark Blue/Grey
   secondary: '#2A9A89', // Secondary Green/Aqua
-  neutralDark: '#323F4B', // Dark Grey
+  neutralDark: '#21262b', // Dark Grey
   neutralMedium: '#7B8794', // Neutral Grey
   neutralLight: '#CBD2D9', // Light Grey
   negative: '#FF3C38', // Red
