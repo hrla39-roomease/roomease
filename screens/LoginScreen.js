f4919313dd71562acb7964909868f00a97a819dc
nickrotondo <nickrotondo@gmail.com>
2020-11-07 21:27:38 -0800

    Style login page

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,10 +1,21 @@
-
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button, SafeAreaView } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+  SafeAreaView,
+  Image,
+  TouchableHighlight,
+  TouchableOpacity,
+} from 'react-native';
 import * as Google from "expo-google-app-auth";
 import firebase from 'firebase';
 import axios from 'axios';
+import { FontAwesome5 } from '@expo/vector-icons';
 
+import colors from '../assets/colors.js';
 
 export default class LoginScreen extends Component {
   isUserEqual = (googleUser, firebaseUser) => {
@@ -83,8 +94,6 @@ export default class LoginScreen extends Component {
     });
   }
 
-
-
   signInWithGoogleAsync = async () => {
     try {
       const result = await Google.logInAsync({
@@ -104,20 +113,147 @@ export default class LoginScreen extends Component {
     }
   };
 
-
   render() {
     return (
-    <SafeAreaView style={styles.container}>
-      <Button title="Sign In With Google" onPress={() => this.signInWithGoogleAsync()}/>
-    </SafeAreaView>
+      <View style={styles.container}>
+        <Image
+          style={styles.logo}
+          source={require('../assets/roomease_logo.png')}
+        />
+        <TextInput
+          style={styles.inputField}
+          onChangeText={text => setItemName(text)}
+          value={''}
+          autoCapitalize={'words'}
+          placeholder={'Email or Username'}
+        />
+        <TextInput
+          style={styles.inputField}
+          onChangeText={text => setItemName(text)}
+          value={''}
+          autoCapitalize={'words'}
+          placeholder={'Password'}
+        />
+        <View style={styles.forgotPassword}>
+          <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+        </View>
+        <TouchableOpacity
+          onPress={() => { }}
+          style={styles.loginButton}
+          underlayColor={colors.primaryLighter}
+        >
+          <View style={styles.buttonContainer}>
+            <Text style={styles.loginButtonText}>Log In</Text>
+          </View>
+        </TouchableOpacity>
+        <View style={styles.orDivider}>
+          <View style={styles.divider}></View>
+          <Text style={styles.orText}>OR</Text>
+          <View style={styles.divider}></View>
+        </View>
+        <TouchableHighlight
+          onPress={() => this.signInWithGoogleAsync()}
+          style={styles.googleLoginButton}
+          underlayColor={colors.primaryLighter}
+        >
+          <View style={styles.buttonContainer}>
+            <FontAwesome5 name="google" size={18} color={colors.primary} />
+            <Text style={styles.googleLoginText}>Continue with Google</Text>
+          </View>
+        </TouchableHighlight>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: "cover",
+  },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  logo: {
+    width: 274.5,
+    height: 39,
+    marginBottom: 38,
+  },
+  inputField: {
+    fontSize: 14,
+    height: 45,
+    borderColor: colors.neutralLight,
+    borderRadius: 25,
+    borderWidth: 1,
+    width: '100%',
+    marginBottom: 0,
+    marginTop: 10,
+    paddingLeft: 22,
+  },
+  forgotPassword: {
+    width: '91%',
+    alignItems: 'flex-end',
+    margin: 12,
+  },
+  forgotPasswordText: {
+    color: colors.primary,
+  },
+  loginButton: {
+    backgroundColor: colors.primaryLight,
+    borderRadius: 25,
+    width: '100%',
+    height: 45,
+    elevation: 2,
+    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loginButtonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: '600',
+  },
+  orDivider: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 22,
+    marginBottom: 19,
+    width: '91%'
+  },
+  orText: {
+    color: colors.neutralMedium,
+    fontSize: 14,
+    fontWeight: '600',
+    flex: 1,
+    textAlign: 'center',
+  },
+  divider: {
+    borderTopWidth: .6,
+    flex: 2,
+    borderColor: colors.neutralMedium,
+  },
+  googleLoginButton: {
+    borderRadius: 25,
+    borderWidth: 1,
+    borderColor: colors.primary,
+    width: '100%',
+    height: 45,
+    elevation: 2,
+    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  googleLoginText: {
+    color: colors.primary,
+    marginLeft: 8,
+    fontSize: 18,
+    fontWeight: '600',
   },
 });
